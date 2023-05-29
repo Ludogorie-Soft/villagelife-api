bbbeb03bd00f0e68ea8b1d3add40e3100acf958f
berki0 <107307903+berki0@users.noreply.github.com>
2023-05-29 21:51:21 +0300

    village
    add village modul

---
+++ village/src/test/java/com/example/ludogorieSoft/village/VillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ludogorieSoft.village;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
