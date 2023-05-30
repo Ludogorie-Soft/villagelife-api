fffd843acaa74d7fbf44788ed546a6a57ba4a496
Berkant <berki9@icloud.com>
2023-05-30 19:57:25 +0300

    test action whit spring.datasource.url=jdbc:mysql://${MYSQL_HOST:localhost}:3306/village

--- src/main/java/com/example/ludogorieSoft/village/VillageApplication.java
+++ src/main/java/com/example/ludogorieSoft/village/VillageApplication.java
@@ -2,9 +2,8 @@ package com.example.ludogorieSoft.village;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={ DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class VillageApplication {
 
 	public static void main(String[] args) {
