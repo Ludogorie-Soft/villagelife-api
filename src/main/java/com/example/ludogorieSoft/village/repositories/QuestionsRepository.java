77c2113302bc152afa146f0d237bfdb0b54f7918
Yumi <yumi_1991_@abv.bg>
2023-05-31 23:50:10 +0300

    create repositories

---
+++ src/main/java/com/example/ludogorieSoft/village/repositories/QuestionsRepository.java
@@ -0,0 +1,7 @@
+package com.example.ludogorieSoft.village.repositories;
+
+import com.example.ludogorieSoft.village.Model.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionsRepository extends JpaRepository<Questions, Long> {
+}
