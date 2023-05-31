eb2f6d93e542e6365140038c2a3a383244ab37cd
Nadezhda Todorova <ntodor358@gmail.com>
2023-05-31 23:49:53 +0300

    add AdministratorRepository, ApprovedVillageRepository, EthnicityRepository, EthnicityVillageRepository, GroundCategoryRepository, LandscapeRepository

---
+++ src/main/java/com/example/ludogorieSoft/village/Model/GroundCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.ludogorieSoft.village.Model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroundCategoryRepository extends JpaRepository<GroundCategory, Long> {
+}
