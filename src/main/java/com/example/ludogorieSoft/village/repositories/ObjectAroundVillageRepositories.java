4aaec4102d25f20d1b1fbf67ca7befbbe89cc0c3
diddka <danailova_rz@abv.bg>
2023-05-31 23:50:11 +0300

    create_some_repositories

---
+++ src/main/java/com/example/ludogorieSoft/village/repositories/ObjectAroundVillageRepositories.java
@@ -0,0 +1,8 @@
+package com.example.ludogorieSoft.village.repositories;
+
+import com.example.ludogorieSoft.village.Model.LivingConditions;
+import com.example.ludogorieSoft.village.Model.ObjectAroundVillage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObjectAroundVillageRepositories extends JpaRepository<ObjectAroundVillage,Long> {
+}
---
+++ src/main/java/com/example/ludogorieSoft/village/repositories/ObjectVillageRepositories.java
@@ -0,0 +1,7 @@
+package com.example.ludogorieSoft.village.repositories;
+
+import com.example.ludogorieSoft.village.Model.ObjectVillage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObjectVillageRepositories extends JpaRepository<ObjectVillage,Long> {
+}
