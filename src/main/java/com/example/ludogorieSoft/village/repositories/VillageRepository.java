c7608fbda4c99c1e1b88deefea0285f48fecc8ab
Nadezhda Todorova <ntodor358@gmail.com>
2023-05-31 23:40:39 +0300

    add VillageRepository

---
+++ src/main/java/com/example/ludogorieSoft/village/repositories/VillageRepository.java
@@ -0,0 +1,7 @@
+package com.example.ludogorieSoft.village.repositories;
+
+import com.example.ludogorieSoft.village.Model.Village;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VillageRepository extends JpaRepository<Village, Long> {
+}
