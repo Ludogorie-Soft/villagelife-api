3b8e3ce61a8a96f54ad2354db454e55bb4268f4a
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-02 17:27:25 +0300

    add LandscapeController and LandscapeService

--- src/main/java/com/example/ludogorieSoft/village/Repositories/LandscapeRepository.java
+++ src/main/java/com/example/ludogorieSoft/village/Repositories/LandscapeRepository.java
@@ -4,4 +4,5 @@ import com.example.ludogorieSoft.village.Model.Landscape;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LandscapeRepository extends JpaRepository<Landscape, Long> {
+    boolean existsByLandscapeName(String landscapeName);
 }
---
+++ src/main/java/com/example/ludogorieSoft/village/Services/LandscapeService.java
@@ -0,0 +1,73 @@
+package com.example.ludogorieSoft.village.Services;
+
+import com.example.ludogorieSoft.village.DTOs.LandscapeDTO;
+import com.example.ludogorieSoft.village.Model.Landscape;
+import com.example.ludogorieSoft.village.Repositories.GroundCategoryRepository;
+import com.example.ludogorieSoft.village.Repositories.LandscapeRepository;
+import com.example.ludogorieSoft.village.exeptions.ApiRequestException;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class LandscapeService {
+    private final LandscapeRepository landscapeRepository;
+    private final ModelMapper modelMapper;
+    public LandscapeDTO landscapeToLandscapeDTO(Landscape landscape) {
+        return modelMapper.map(landscape, LandscapeDTO.class);
+    }
+
+    public List<LandscapeDTO> getAllLandscapes() {
+        List<Landscape> landscapes = landscapeRepository.findAll();
+        return landscapes
+                .stream()
+                .map(this::landscapeToLandscapeDTO)
+                .collect(Collectors.toList());
+    }
+
+    public LandscapeDTO getByLandscapeId(Long id) {
+        Optional<Landscape> optionalLandscape = landscapeRepository.findById(id);
+        if (optionalLandscape.isEmpty()) {
+            throw new ApiRequestException("Landscape with id: " + id + " Not Found");
+        }
+        return landscapeToLandscapeDTO(optionalLandscape.get());
+    }
+    public LandscapeDTO createLandscape(LandscapeDTO landscapeDTO) {
+        if (landscapeRepository.existsByLandscapeName(landscapeDTO.getLandscapeName())) {
+            throw new ApiRequestException("Landscape with name: " + landscapeDTO.getLandscapeName() + " already exists");
+        }
+
+        Landscape landscape = new Landscape();
+        landscape.setLandscapeName(landscapeDTO.getLandscapeName());
+        landscapeRepository.save(landscape);
+
+        return landscapeDTO;
+    }
+    public LandscapeDTO updateLandscape(Long id, Landscape landscape) {
+        Optional<Landscape> foundLandscape = landscapeRepository.findById(id);
+        if (foundLandscape.isEmpty()) {
+            throw new ApiRequestException("Landscape with id: " + id + " Not Found");
+        }
+        if (landscapeRepository.existsByLandscapeName(landscape.getLandscapeName())) {
+            throw new ApiRequestException("Landscape with name: " + landscape.getLandscapeName() + " already exists");
+        }
+        foundLandscape.get().setLandscapeName(landscape.getLandscapeName());
+
+        landscapeRepository.save(foundLandscape.get());
+        return landscapeToLandscapeDTO(foundLandscape.get());
+    }
+
+    public void deleteLandscape(Long id) {
+        Optional<Landscape> landscape = landscapeRepository.findById(id);
+        if (landscape.isEmpty()) {
+            throw new ApiRequestException("Landscape not found for id " + id);
+        }
+        landscapeRepository.delete(landscape.get());
+    }
+
+}
