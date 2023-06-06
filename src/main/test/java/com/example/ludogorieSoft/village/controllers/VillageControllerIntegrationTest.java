4c87fa3cafe027dbf62f24c3eab781a4804d4875
Berkant <berki9@icloud.com>
2023-06-06 22:56:30 +0300

    add VillageControllerIntegrationTest.java

---
+++ src/main/test/java/com/example/ludogorieSoft/village/controllers/VillageControllerIntegrationTest.java
@@ -0,0 +1,136 @@
+package com.example.ludogorieSoft.village.controllers;
+import com.example.ludogorieSoft.village.dtos.VillageDTO;
+import com.example.ludogorieSoft.village.services.VillageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(VillageController.class)
+@AutoConfigureMockMvc
+public class VillageControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private VillageService villageService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllVillages() throws Exception {
+        // Arrange
+        VillageDTO villageDTO1 = new VillageDTO();
+        villageDTO1.setId(1L);
+        VillageDTO villageDTO2 = new VillageDTO();
+        villageDTO2.setId(2L);
+        List<VillageDTO> villageDTOList = Arrays.asList(villageDTO1, villageDTO2);
+
+        when(villageService.getAllVillages()).thenReturn(villageDTOList);
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/villages")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(1))
+                .andExpect(jsonPath("$.[1].id").value(2))
+                .andReturn();
+
+        // Assert
+        String response = mvcResult.getResponse().getContentAsString();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testGetVillageById() throws Exception {
+        // Arrange
+        VillageDTO villageDTO = new VillageDTO();
+        villageDTO.setId(1L);
+
+        when(villageService.getVillageById(anyLong())).thenReturn(villageDTO);
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/villages/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        // Assert
+        String response = mvcResult.getResponse().getContentAsString();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testCreateVillage() throws Exception {
+        // Arrange
+        VillageDTO villageDTO = new VillageDTO();
+        villageDTO.setId(1L);
+
+        when(villageService.createVillage(any(VillageDTO.class))).thenReturn(villageDTO);
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/villages")
+                        .content("{\"id\": 1}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        // Assert
+        String response = mvcResult.getResponse().getContentAsString();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testUpdateVillage() throws Exception {
+        // Arrange
+        VillageDTO villageDTO = new VillageDTO();
+        villageDTO.setId(1L);
+
+        when(villageService.updateVillage(anyLong(), any(VillageDTO.class))).thenReturn(villageDTO);
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/villages/{id}", 1)
+                        .content("{\"id\": 1}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        // Assert
+        String response = mvcResult.getResponse().getContentAsString();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testDeleteVillage() throws Exception {
+        // Arrange
+
+        // Act
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/villages/{id}", 1))
+                .andExpect(status().isNoContent());
+    }
+}
+
