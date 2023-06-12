e7f5b8bca3475c515a763e92f9fcc8089bea50c7
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-12 22:32:43 +0300

    edit migrations

--- src/main/resources/db/migration/V16__create_table_villageLivingConditions.sql
+++ src/main/resources/db/migration/V16__create_table_villageLivingConditions.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS village_living_conditions (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     village_id BIGINT,
     living_condition_id BIGINT,
-    consents INTEGER,
+    consents VARCHAR(255) NOT NULL,
     FOREIGN KEY (village_id) REFERENCES villages(id),
     FOREIGN KEY (living_condition_id) REFERENCES living_conditions(id)
 );
