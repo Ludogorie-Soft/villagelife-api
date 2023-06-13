1ed7d9a5cfedc11518cd08765f74fe449538f4d7
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-14 01:53:48 +0300

    add tests for method findEthnicityByName and test it

--- src/main/java/com/example/ludogorieSoft/village/repositories/EthnicityRepository.java
+++ src/main/java/com/example/ludogorieSoft/village/repositories/EthnicityRepository.java
@@ -1,7 +1,6 @@
 package com.example.ludogoriesoft.village.repositories;
 
 import com.example.ludogoriesoft.village.model.Ethnicity;
-import com.example.ludogoriesoft.village.model.GroundCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +8,6 @@ import java.util.List;
 public interface EthnicityRepository extends JpaRepository<Ethnicity, Long> {
     boolean existsByEthnicityName(String ethnicityName);
     List<Ethnicity> findAllByOrderByIdAsc();
+
+    Ethnicity findByEthnicityName(String name);
 }
