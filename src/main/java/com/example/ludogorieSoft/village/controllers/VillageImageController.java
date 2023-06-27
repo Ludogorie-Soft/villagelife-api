e47a40ca9cf54922ae6104ab87c38800ff3cddaf
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-27 18:34:28 +0300

    add method for getting and image in VillageImageController

--- src/main/java/com/example/ludogorieSoft/village/controllers/VillageImageController.java
+++ src/main/java/com/example/ludogorieSoft/village/controllers/VillageImageController.java
@@ -3,23 +3,43 @@ package com.example.ludogoriesoft.village.controllers;
 
 import com.example.ludogoriesoft.village.services.VillageImageService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 @RestController
 @RequestMapping("/api/v1/villageImages")
 @AllArgsConstructor
 public class VillageImageController {
-    public final VillageImageService villageImageService;
-    @GetMapping("/getImageBytesFromMultipartFile")
-    public ResponseEntity<List<byte[]>> getImageBytesFromMultipartFile(@RequestBody List<MultipartFile> images) {
-        List<byte[]> imageBytes = villageImageService.getImageBytesFromMultipartFile(images);
-        return ResponseEntity.ok(imageBytes);
+    private final VillageImageService villageImageService;
+
+    @GetMapping("/{imageName}")
+    public ResponseEntity<Resource> getImage(@PathVariable String imageName) {
+        String imageDirectory = "src/main/resources/static/village_images/"; // Replace with the actual directory path where the images are stored
+        String imagePath = imageDirectory + imageName;
+
+        try {
+            Resource imageResource = new UrlResource("file:" + imagePath);
+            if (imageResource.exists()) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.IMAGE_JPEG);
+                return ResponseEntity.ok().headers(headers).body(imageResource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
 }
