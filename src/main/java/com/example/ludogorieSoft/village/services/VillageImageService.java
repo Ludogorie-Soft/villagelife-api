1a284a68a067d6180de48ab830553922375a1857
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-30 18:46:26 +0300

    try to fix sonar security problem

--- src/main/java/com/example/ludogorieSoft/village/services/VillageImageService.java
+++ src/main/java/com/example/ludogorieSoft/village/services/VillageImageService.java
@@ -7,6 +7,8 @@ import com.example.ludogorieSoft.village.repositories.VillageImageRepository;
 import lombok.AllArgsConstructor;
 import org.apache.tika.Tika;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +26,7 @@ public class VillageImageService {
     private static final String UPLOAD_DIRECTORY = "src/main/resources/static/village_images";
     private final ModelMapper modelMapper;
     private final VillageService villageService;
+    private static final Logger logger = LoggerFactory.getLogger(VillageImageService.class);
 
     public List<String> createImagePaths(List<byte[]> imageBytes, Long villageId) {
         List<String> imagePaths = new ArrayList<>();
@@ -53,7 +56,7 @@ public class VillageImageService {
             writeImageToFile(image, fullPath);
             return fileName;
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("An error occurred while processing the image", e);
             return null;
         }
     }
@@ -95,14 +98,14 @@ public class VillageImageService {
     public VillageImage villageImageDTOToVillageImage(VillageImageDTO villageImageDTO) {
         return modelMapper.map(villageImageDTO, VillageImage.class);
     }
-    public List<byte[]> getImageBytesFromMultipartFile(List<MultipartFile> images){
+    public List<byte[]> getImageBytesFromMultipartFile(List<MultipartFile> images) {
         List<byte[]> imageBytes = new ArrayList<>();
         for (MultipartFile image : images) {
             try {
                 byte[] imageData = image.getBytes();
                 imageBytes.add(imageData);
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("An error occurred while getting image bytes", e);
             }
         }
         return imageBytes;
