34741226a0131d8e40515cfb884f058421364199
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-30 18:09:21 +0300

    separate the createImagePaths into smaller methods

--- src/main/java/com/example/ludogorieSoft/village/services/VillageImageService.java
+++ src/main/java/com/example/ludogorieSoft/village/services/VillageImageService.java
@@ -5,13 +5,11 @@ import com.example.ludogorieSoft.village.model.Village;
 import com.example.ludogorieSoft.village.model.VillageImage;
 import com.example.ludogorieSoft.village.repositories.VillageImageRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.apache.tika.Tika;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -31,34 +29,57 @@ public class VillageImageService {
         List<String> imagePaths = new ArrayList<>();
         for (byte[] image : imageBytes) {
             if (image.length > 0) {
-                try {
-                    Tika tika = new Tika();
-                    String mimeType = tika.detect(image);
-                    if (!mimeType.startsWith("image/")) {
-                        imagePaths.add("Invalid file format. Only images are allowed.");
-                        continue;
-                    }
-                    String fileName = UUID.randomUUID() + ".jpg";
-                    String filePath = getUploadDirectoryPath();
-                    File uploadDir = new File(filePath);
-                    if (!uploadDir.exists()) {
-                        uploadDir.mkdirs();
-                    }
-                    String fullPath = filePath + File.separator + fileName;
-                    FileOutputStream fos = new FileOutputStream(fullPath);
-                    fos.write(image);
-                    fos.close();
-                    imagePaths.add(fileName);
-
-                    VillageImageDTO villageImageDTO = new VillageImageDTO(null, villageId, fileName);
-                    createVillageImageDTO(villageImageDTO);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                String imagePath = processImage(image);
+                if (imagePath != null) {
+                    imagePaths.add(imagePath);
+                    createVillageImageDTO(villageId, imagePath);
                 }
             }
         }
         return imagePaths;
     }
+
+    private String processImage(byte[] image) {
+        try {
+            Tika tika = new Tika();
+            String mimeType = tika.detect(image);
+            if (!mimeType.startsWith("image/")) {
+                return null;
+            }
+            String fileName = generateFileName();
+            String filePath = getUploadDirectoryPath();
+            createUploadDirectory(filePath);
+            String fullPath = filePath + File.separator + fileName;
+            writeImageToFile(image, fullPath);
+            return fileName;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String generateFileName() {
+        return UUID.randomUUID() + ".jpg";
+    }
+
+    private void createUploadDirectory(String filePath) {
+        File uploadDir = new File(filePath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+    }
+
+    private void writeImageToFile(byte[] image, String fullPath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(fullPath)) {
+            fos.write(image);
+        }
+    }
+
+    private void createVillageImageDTO(Long villageId, String fileName) {
+        VillageImageDTO villageImageDTO = new VillageImageDTO(null, villageId, fileName);
+        createVillageImageDTO(villageImageDTO);
+    }
+
     public String getUploadDirectoryPath() {
         String currentPath = System.getProperty("user.dir");
         return currentPath + File.separator + UPLOAD_DIRECTORY;
