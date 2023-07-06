3e4668aee393813c944cb7e8009e14a2e034c1fc
Nadezhda Todorova <ntodor358@gmail.com>
2023-07-06 23:42:06 +0300

    add testConfigureMailSender

--- src/test/java/com/example/ludogoriesoft/village/services/EmailSenderServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/village/services/EmailSenderServiceTest.java
@@ -1,19 +1,36 @@
 package com.example.ludogorieSoft.village.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
-import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
+
+import java.util.Properties;
 
 class EmailSenderServiceTest {
+
+    @InjectMocks
+    EmailSenderService emailSenderService;
     @Mock
     private JavaMailSender mailSender;
-    @InjectMocks
-    private EmailSenderService emailSenderService;
+
     @Mock
     private Logger logger;
+
     @Value("${spring.mail.username}")
     private String recipientEmail;
 
@@ -24,6 +41,28 @@ class EmailSenderServiceTest {
     public void setup() {
         MockitoAnnotations.openMocks(this);
     }
+    @Test
+    void testConfigureMailSender() {
+        JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+        mailSenderImpl.setHost("smtp.example.com");
+        mailSenderImpl.setPort(25);
 
+        mailSenderImpl.setUsername(recipientEmail);
+        mailSenderImpl.setPassword(recipientPassword);
 
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.auth", "true");
+        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+
+        mailSenderImpl.setJavaMailProperties(javaMailProperties);
+
+        emailSenderService.configureMailSender();
+
+        Assertions.assertEquals(recipientEmail, mailSenderImpl.getUsername());
+        Assertions.assertEquals(recipientPassword, mailSenderImpl.getPassword());
+
+        Properties configuredProperties = mailSenderImpl.getJavaMailProperties();
+        Assertions.assertEquals("true", configuredProperties.getProperty("mail.smtp.auth"));
+        Assertions.assertEquals("true", configuredProperties.getProperty("mail.smtp.starttls.enable"));
+    }
 }
