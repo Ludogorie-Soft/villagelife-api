fd1c0c8fa816679eb776103f55655a4653f3f100
diddka <danailova_rz@abv.bg>
2023-07-13 21:58:38 +0300

    fix sonar hotspot

--- src/main/java/com/example/ludogorieSoft/village/authorization/SecurityConfiguration.java
+++ src/main/java/com/example/ludogorieSoft/village/authorization/SecurityConfiguration.java
@@ -3,12 +3,16 @@ package com.example.ludogorieSoft.village.authorization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +26,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
-                .disable()
+                .requireCsrfProtectionMatcher(new AndRequestMatcher(
+                        CsrfFilter.DEFAULT_CSRF_MATCHER,
+                        new RequestHeaderRequestMatcher(HttpHeaders.COOKIE)))
+                .and()
                 .cors().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/admins/**", "/api/v1/auth/register")
