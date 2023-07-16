7e56dd677214da3cebafc34d55aec517b96beed3
diddka <danailova_rz@abv.bg>
2023-07-16 16:34:59 +0300

    add tests

--- src/test/java/com/example/ludogoriesoft/village/authorization/JWTServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/village/authorization/JWTServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
@@ -16,14 +17,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class JWTServiceTest {
     @Mock
     private JWTService jwtService;
-
     @Mock
     private UserDetails userDetails;
 
@@ -31,10 +31,49 @@ class JWTServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         jwtService = new JWTService("Y9fX8iAVeahSE4RDhyC4jqUlxvJa0g+zz7u5tJXj6ZObDsKJLiUi4GROECghCfEd");
+                userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn("testuser");
     }
 
 
+    @Test
+    void testExtractUsername() {
+        String token = generateToken();
+        String username = jwtService.extractUsername(token);
+        assertEquals("testuser", username);
+    }
 
+    @Test
+    void testGenerateToken() {
+        String token = jwtService.generateToken(userDetails);
+        assertNotNull(token);
+        assertTrue(token.length() > 0);
+    }
+
+    @Test
+    void testIsTokenValid() {
+        String token = generateToken();
+        boolean isValid = jwtService.isTokenValid(token, userDetails);
+        assertTrue(isValid);
+    }
+
+    @Test
+    void testIsTokenExpired() {
+        String token = generateToken();
+        boolean isExpired = jwtService.isTokenExpired(token);
+        assertFalse(isExpired);
+    }
+
+    @Test
+    void testExtractExpiration() {
+        String token = generateToken();
+        Date expiration = jwtService.extractExpiration(token);
+        assertNotNull(expiration);
+    }
+
+    private String generateToken() {
+        return jwtService.generateToken(new HashMap<>(), userDetails);
+    }
     @Test
     void testGetSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtService.getSecretKey());
