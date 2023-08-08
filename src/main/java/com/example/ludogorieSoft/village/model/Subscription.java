b4500c6233e7d0e383206663b65fde341581f110
Nadezhda Todorova <ntodor358@gmail.com>
2023-08-09 00:58:45 +0300

    add Subscription and V34__add_subscriptions_table.sql

---
+++ src/main/java/com/example/ludogorieSoft/village/model/Subscription.java
@@ -0,0 +1,39 @@
+package com.example.ludogorieSoft.village.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "subscriptions")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message = "Email cannot be empty!")
+    @Email(message = "Please enter a valid email address!")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deletedAt;
+}
