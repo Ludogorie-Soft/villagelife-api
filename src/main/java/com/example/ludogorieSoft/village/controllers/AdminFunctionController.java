58ac1bf186784ba3626d56bf733e926b8b397d8e
Nadezhda Todorova <ntodor358@gmail.com>
2023-08-27 00:11:28 +0300

    add method getPopulationByVillageId in AdminFunctionController

--- src/main/java/com/example/ludogorieSoft/village/controllers/AdminFunctionController.java
+++ src/main/java/com/example/ludogorieSoft/village/controllers/AdminFunctionController.java
@@ -1,9 +1,11 @@
 package com.example.ludogorieSoft.village.controllers;
 
+import com.example.ludogorieSoft.village.dtos.PopulationDTO;
 import com.example.ludogorieSoft.village.dtos.VillageDTO;
 import com.example.ludogorieSoft.village.dtos.response.VillageInfo;
 import com.example.ludogorieSoft.village.dtos.response.VillageResponse;
 import com.example.ludogorieSoft.village.services.AdminVillageService;
+import com.example.ludogorieSoft.village.services.PopulationService;
 import com.example.ludogorieSoft.village.services.VillageInfoService;
 import com.example.ludogorieSoft.village.services.VillageService;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,7 @@ public class AdminFunctionController {
     private final VillageService villageService;
     private final AdminVillageService adminVillageService;
     private final VillageInfoService villageInfoService;
+    private PopulationService populationService;
 
     @DeleteMapping("/village-delete/{villageId}")
     public ResponseEntity<String> deleteVillageById(@PathVariable("villageId") Long villageId) {
@@ -54,6 +57,11 @@ public class AdminFunctionController {
                                                           @RequestParam("answerDate") String answerDate, boolean status) {
         VillageInfo villageInfo = villageInfoService.getVillageInfoByVillageId(villageId, status, answerDate);
         return ResponseEntity.ok(villageInfo);
+    }
+    @GetMapping("/population/villageId/{id}")
+    public ResponseEntity<PopulationDTO> getPopulationByVillageId(@PathVariable("id") Long id,
+                                                                  @RequestParam("answerDate") String answerDate, boolean status) {
+        return ResponseEntity.ok(populationService.getPopulationByVillageId(id, status, answerDate));
     }
     @GetMapping("/getRejected")
     public ResponseEntity<List<VillageResponse>> getVillagesWithRejectedResponses() {
