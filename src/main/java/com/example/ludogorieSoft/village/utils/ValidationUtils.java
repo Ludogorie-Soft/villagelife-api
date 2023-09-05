72c1855806f66259e07e476e5e19d6ca92808e53
Yumi <yumi_1991_@abv.bg>
2023-09-05 20:33:46 +0300

    add ValidationUtils class

---
+++ src/main/java/com/example/ludogorieSoft/village/utils/ValidationUtils.java
@@ -0,0 +1,14 @@
+package com.example.ludogorieSoft.village.utils;
+
+public class ValidationUtils {
+    public static boolean isValidName(String name) {
+        String regex = "^[а-яА-Я\\s]+$";
+        return name.matches(regex);
+    }
+
+
+    public static boolean isValidNumber(String number) {
+        String regex = "^[0-9]+$";
+        return number.matches(regex);
+    }
+}
