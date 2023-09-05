781711776c11d3eb0d170c61d04cb4476d901423
Yumi <yumi_1991_@abv.bg>
2023-09-05 21:30:18 +0300

    write tests

--- src/main/java/com/example/ludogorieSoft/village/utils/ValidationUtils.java
+++ src/main/java/com/example/ludogorieSoft/village/utils/ValidationUtils.java
@@ -1,11 +1,14 @@
 package com.example.ludogorieSoft.village.utils;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ValidationUtils {
+
     public static boolean isValidName(String name) {
-        String regex = "^[а-яА-Я\\s]+$";
+        String regex = "^[а-яА-Я\\s\\-]+$";
         return name.matches(regex);
     }
-
 
     public static boolean isValidNumber(String number) {
         String regex = "^[0-9]+$";
