d2881307526a8e6588a18f60e981e2a4927b3cdb
Nadezhda Todorova <ntodor358@gmail.com>
2023-09-10 21:20:40 +0300

    add missing imports

--- src/test/java/com/example/ludogoriesoft/village/services/VillageImageServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/village/services/VillageImageServiceTest.java
@@ -3,12 +3,8 @@ package com.example.ludogorieSoft.village.services;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 import com.example.ludogorieSoft.village.dtos.VillageDTO;
 import com.example.ludogorieSoft.village.dtos.VillageImageDTO;
