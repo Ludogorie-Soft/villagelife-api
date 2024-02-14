4a52e8e094411ffc2181b108cf4541bad1daa83e
Nadezhda Todorova <ntodor358@gmail.com>
2024-02-14 22:15:02 +0200

    edit FilterController

--- src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
+++ src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
@@ -3,7 +3,7 @@ package com.example.ludogorieSoft.village.controllers;
 import com.example.ludogorieSoft.village.dtos.VillageDTO;
 import com.example.ludogorieSoft.village.enums.Children;
 import com.example.ludogorieSoft.village.services.VillageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +11,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/filter")
+@AllArgsConstructor
 public class FilterController {
-    @Autowired
     private VillageService villageSearchService;
 
 
