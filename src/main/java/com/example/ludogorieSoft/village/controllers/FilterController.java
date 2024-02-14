f609ee5b1eddf538f929c97a565f2eca19f33405
Nadezhda Todorova <ntodor358@gmail.com>
2024-02-14 21:46:47 +0200

    add tests in FilterControllerIntegrationTest

--- src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
+++ src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
@@ -4,7 +4,6 @@ import com.example.ludogorieSoft.village.dtos.VillageDTO;
 import com.example.ludogorieSoft.village.enums.Children;
 import com.example.ludogorieSoft.village.services.VillageService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/ludogorieSoft/village/services/EmailSenderService.java
+++ src/main/java/com/example/ludogorieSoft/village/services/EmailSenderService.java
@@ -1,11 +1,9 @@
 package com.example.ludogorieSoft.village.services;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
