baead6b8d8c928ea87a5eda4fb10a1f255bf7f28
diddka <danailova_rz@abv.bg>
2024-03-13 23:23:45 +0200

    add LivingConditions  and ground_categories tables - keys

---
+++ src/main/resources/db/migration/V40__update_tables_translation_keys_1.sql
@@ -0,0 +1,26 @@
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.first' WHERE `id` = 1;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.second' WHERE `id` = 2;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.third' WHERE `id` = 3;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.fourth' WHERE `id` = 4;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.fifth' WHERE `id` = 5;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.sixth' WHERE `id` = 6;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.seventh' WHERE `id` = 7;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.eight' WHERE `id` = 8;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.ninth' WHERE `id` = 9;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.tenth' WHERE `id` = 10;
+UPDATE `ground_categories` SET `ground_category_name` = 'ground_categories.doNotKnow' WHERE `id` = 11;
+
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.first' WHERE `id` = 1;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.second' WHERE `id` = 2;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.third' WHERE `id` = 3;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.fourth' WHERE `id` = 4;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.fifth' WHERE `id` = 5;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.sixth' WHERE `id` = 6;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.seventh' WHERE `id` = 7;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.eight' WHERE `id` = 8;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.ninth' WHERE `id` = 9;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.tenth' WHERE `id` = 10;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.eleventh' WHERE `id` = 11;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.twelfth' WHERE `id` = 12;
+UPDATE `living_conditions` SET `living_condition_name` = 'living_condition.thirteenth' WHERE `id` = 13;
+
