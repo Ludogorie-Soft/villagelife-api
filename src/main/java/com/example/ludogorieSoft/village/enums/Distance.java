13b18c8a8287615e2e81ccf1493ea098dadf776c
diddka <danailova_rz@abv.bg>
2024-03-16 16:29:58 +0200

    add distance keys

--- src/main/java/com/example/ludogorieSoft/village/enums/Distance.java
+++ src/main/java/com/example/ludogorieSoft/village/enums/Distance.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Distance {
-    IN_THE_VILLAGE("distance.one", 100),
-    ON_10_KM("distance.two", 80),
-    ON_11_TO_30KM("distance.three", 60),
-    ON_31_TO_50_KM("distance.four", 40),
-    OVER_50_KM("distance.five", 20);
+    IN_THE_VILLAGE("distance.in_the_village", 100),
+    ON_10_KM("distance.on_10_km", 80),
+    ON_11_TO_30KM("distance.on_11_to_30km", 60),
+    ON_31_TO_50_KM("distance.on_31_to_50_km", 40),
+    OVER_50_KM("distance.over_50_km", 20);
 
     private final String name;
     private final int value;
