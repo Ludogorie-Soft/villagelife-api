b8c5d40f51eddaa2c176e95877da270e2ad3f97c
Nadezhda Todorova <ntodor358@gmail.com>
2024-04-14 20:15:18 +0300

    add dateSent to InquiryDTO and MessageDTO

--- src/main/java/com/example/ludogorieSoft/village/dtos/InquiryDTO.java
+++ src/main/java/com/example/ludogorieSoft/village/dtos/InquiryDTO.java
@@ -1,7 +1,14 @@
 package com.example.ludogorieSoft.village.dtos;
 
 import com.example.ludogorieSoft.village.enums.InquiryType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,4 +22,8 @@ public class InquiryDTO {
     private String mobile;
     private Long villageId;
     private InquiryType inquiryType;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dateSent;
 }
--- src/main/java/com/example/ludogorieSoft/village/dtos/MessageDTO.java
+++ src/main/java/com/example/ludogorieSoft/village/dtos/MessageDTO.java
@@ -1,7 +1,15 @@
 package com.example.ludogorieSoft.village.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 public class MessageDTO {
@@ -9,4 +17,8 @@ public class MessageDTO {
     private String userName;
     private String email;
     private String userMessage;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dateSent;
 }
