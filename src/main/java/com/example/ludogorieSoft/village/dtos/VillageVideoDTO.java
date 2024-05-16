787f12b7e915c71ccf96a7012a8a8a6c7cc43893
Nadezhda Todorova <ntodor358@gmail.com>
2024-05-16 19:55:22 +0300

    add annotations for date in VillageVideoDTO

--- src/main/java/com/example/ludogorieSoft/village/dtos/VillageVideoDTO.java
+++ src/main/java/com/example/ludogorieSoft/village/dtos/VillageVideoDTO.java
@@ -1,5 +1,10 @@
 package com.example.ludogorieSoft.village.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +17,13 @@ public class VillageVideoDTO {
         private Long villageId;
         private String url;
         private Boolean status;
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
         private LocalDateTime dateUpload;
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
         private LocalDateTime dateDeleted;
         private UserDTO userDTO;
 }
