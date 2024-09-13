115fc2576f60929655a55dbc0be542334219b374
Nadezhda Todorova <ntodor358@gmail.com>
2024-09-13 18:06:21 +0300

    add BusinessCardDTO and PropertyDTO

---
+++ src/main/java/com/example/ludogorieSoft/village/dtos/BusinessCardDTO.java
@@ -0,0 +1,35 @@
+package com.example.ludogorieSoft.village.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BusinessCardDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String websiteLink;
+    private int numberOfEmployees;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/com/example/ludogorieSoft/village/dtos/PropertyDTO.java
@@ -0,0 +1,60 @@
+package com.example.ludogorieSoft.village.dtos;
+
+import com.example.ludogorieSoft.village.enums.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PropertyDTO {
+    private Long id;
+    private VillageDTO villageDTO;
+    private PropertyUserDTO propertyUserDTO;
+    private PropertyType propertyType;
+    private PropertyTransferType propertyTransferType;
+    private BigDecimal price;
+    private String phoneNumber;
+    private Double buildUpArea;
+    private Double yardArea;
+    private int roomsCount;
+    private int bathroomsCount;
+    private List<String> heating;
+    private String imageUrl;
+    private ConstructionType constructionType;
+    private int constructionYear;
+    private String extras;
+    private PropertyStatsDTO propertyStatsDTO;
+    private OwnershipType ownershipType;
+    private PropertyCondition propertyCondition;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime createdAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime updatedAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deactivatedAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deletedAt;
+
+}
