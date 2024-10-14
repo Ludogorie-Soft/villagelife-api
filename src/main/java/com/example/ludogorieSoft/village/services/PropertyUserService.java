be6dc2d7486774ffecb258ad243359205525fe9b
TodorYanakiev <todoryanakiev395@gmail.com>
2024-10-14 11:33:29 +0300

    Remove PropertyUser

--- src/main/java/com/example/ludogorieSoft/village/services/PropertyUserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ludogorieSoft.village.services;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class PropertyUserService {
-}
