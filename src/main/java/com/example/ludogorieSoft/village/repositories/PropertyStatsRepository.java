132a46f6a87e40dcc05f828eaffaf61bf6b7cafa
Nadezhda Todorova <ntodor358@gmail.com>
2024-11-28 20:17:00 +0200

    pull from develop

--- src/main/java/com/example/ludogorieSoft/village/repositories/PropertyStatsRepository.java
+++ src/main/java/com/example/ludogorieSoft/village/repositories/PropertyStatsRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface PropertyStatsRepository extends JpaRepository<PropertyStats,Long> {
     @Transactional
     @Modifying
--- src/main/java/com/example/ludogorieSoft/village/services/PropertyService.java
+++ src/main/java/com/example/ludogorieSoft/village/services/PropertyService.java
@@ -116,7 +116,8 @@ public class PropertyService {
         addMainImageToPropertyDTO(propertyDTO);
         return propertyDTO;
     }
-    public Property getPropertyById(Long id){
+
+    public Property getPropertyById(Long id) {
         Optional<Property> optionalProperty = propertyRepository.findById(id);
         if (optionalProperty.isEmpty()) {
             throw new ApiRequestException("Property with id: " + id + " Not Found");
@@ -124,114 +125,114 @@ public class PropertyService {
         return optionalProperty.get();
     }
 
-        public Page<PropertyDTO> getSearchProperties (List < String > propertyTypes, String propertyTransferType,
-                Double minBuiltUpArea, Double maxBuiltUpArea, Double minYardArea,
-                Double maxYardArea, Short minRoomsCount, Short maxRoomsCount,
-                Short minBathroomsCount, Short maxBathroomsCount, List < String > heating,
-                List < String > constructionTypes, Short minConstructionYear,
-                Short maxConstructionYear, BigDecimal minPrice, BigDecimal maxPrice,
-                List < String > ownershipTypes, String villageName, String regionName,
-                Pageable pageable){
+    public Page<PropertyDTO> getSearchProperties(List<String> propertyTypes, String propertyTransferType,
+                                                 Double minBuiltUpArea, Double maxBuiltUpArea, Double minYardArea,
+                                                 Double maxYardArea, Short minRoomsCount, Short maxRoomsCount,
+                                                 Short minBathroomsCount, Short maxBathroomsCount, List<String> heating,
+                                                 List<String> constructionTypes, Short minConstructionYear,
+                                                 Short maxConstructionYear, BigDecimal minPrice, BigDecimal maxPrice,
+                                                 List<String> ownershipTypes, String villageName, String regionName,
+                                                 Pageable pageable) {
 
-            List<PropertyType> propertyTypesValues = mapToPropertyTypeList(propertyTypes);
-            PropertyTransferType propertyTransferTypeValue = mapToPropertyTransferType(propertyTransferType);
-            List<ConstructionType> constructionTypesValues = mapToConstructionTypeList(constructionTypes);
-            List<OwnershipType> ownershipTypesValues = mapToOwnershipTypeList(ownershipTypes);
+        List<PropertyType> propertyTypesValues = mapToPropertyTypeList(propertyTypes);
+        PropertyTransferType propertyTransferTypeValue = mapToPropertyTransferType(propertyTransferType);
+        List<ConstructionType> constructionTypesValues = mapToConstructionTypeList(constructionTypes);
+        List<OwnershipType> ownershipTypesValues = mapToOwnershipTypeList(ownershipTypes);
 
-            Page<Property> properties = propertyRepository.searchProperties(
-                    propertyTypesValues, propertyTransferTypeValue, minBuiltUpArea, maxBuiltUpArea, minYardArea, maxYardArea,
-                    minRoomsCount, maxRoomsCount, minBathroomsCount, maxBathroomsCount, heating, constructionTypesValues,
-                    minConstructionYear, maxConstructionYear, minPrice, maxPrice, ownershipTypesValues, villageName, regionName, pageable);
+        Page<Property> properties = propertyRepository.searchProperties(
+                propertyTypesValues, propertyTransferTypeValue, minBuiltUpArea, maxBuiltUpArea, minYardArea, maxYardArea,
+                minRoomsCount, maxRoomsCount, minBathroomsCount, maxBathroomsCount, heating, constructionTypesValues,
+                minConstructionYear, maxConstructionYear, minPrice, maxPrice, ownershipTypesValues, villageName, regionName, pageable);
 
-            return properties.map(this::propertyToPropertyDTO);
-        }
-
-        private List<PropertyType> mapToPropertyTypeList (List < String > propertyTypes) {
-            if (propertyTypes == null) return null;
-            return propertyTypes.stream()
-                    .map(type -> {
-                        try {
-                            return PropertyType.valueOf(type);
-                        } catch (IllegalArgumentException e) {
-                            return null;
-                        }
-                    })
-                    .filter(Objects::nonNull)
-                    .toList();
-        }
-
-        private PropertyTransferType mapToPropertyTransferType (String propertyTransferType){
-            if (propertyTransferType == null) return null;
-            try {
-                return PropertyTransferType.valueOf(propertyTransferType);
-            } catch (IllegalArgumentException e) {
-                return null;
-            }
-        }
-
-        private List<ConstructionType> mapToConstructionTypeList (List < String > constructionTypes) {
-            if (constructionTypes == null) return null;
-            return constructionTypes.stream()
-                    .map(type -> {
-                        try {
-                            return ConstructionType.valueOf(type);
-                        } catch (IllegalArgumentException e) {
-                            return null;
-                        }
-                    })
-                    .filter(Objects::nonNull)
-                    .toList();
-        }
-
-        private List<OwnershipType> mapToOwnershipTypeList (List < String > ownershipTypes) {
-            if (ownershipTypes == null) return null;
-            return ownershipTypes.stream()
-                    .map(type -> {
-                        try {
-                            return OwnershipType.valueOf(type);
-                        } catch (IllegalArgumentException e) {
-                            return null;
-                        }
-                    })
-                    .filter(Objects::nonNull)
-                    .toList();
-        }
-
-        public PropertyDTO createProperty (PropertyDTO propertyDTO){
-            Property property = propertyDTOToProperty(propertyDTO);
-            String imageUUID = randomUUID().toString();
-            String imageName = imageService.uploadImage(propertyDTO.getMainImageBytes(), imageUUID);
-            property.setImageUrl(imageName);
-            List<String> heatingTypes = new ArrayList<>();
-            if (propertyDTO.getHeating() != null) {
-                heatingTypes.addAll(propertyDTO.getHeating());
-            }
-            if (propertyDTO.getHeatingText() != null && !propertyDTO.getHeatingText().trim().isEmpty()) {
-                List<String> additionalHeating = splitHeatingText(propertyDTO.getHeatingText());
-                heatingTypes.addAll(additionalHeating);
-            }
-            property.setHeating(heatingTypes);
-            Property savedProperty = propertyRepository.save(property);
-            propertyImageService.createPropertyImage(propertyDTO.getImages(), savedProperty);
-            return modelMapper.map(savedProperty, PropertyDTO.class);
-        }
-
-        protected List<String> splitHeatingText (String heatingText){
-            if (heatingText == null || heatingText.trim().isEmpty()) {
-                return new ArrayList<>();
-            }
-            Pattern pattern = Pattern.compile("[^;,\\s]{1,100}");
-            Matcher matcher = pattern.matcher(heatingText);
-
-            List<String> heatingTypes = new ArrayList<>();
-            while (matcher.find()) {
-                heatingTypes.add(matcher.group().trim());
-            }
-            return heatingTypes;
-        }
-
-
+        return properties.map(this::propertyToPropertyDTO);
     }
+
+    private List<PropertyType> mapToPropertyTypeList(List<String> propertyTypes) {
+        if (propertyTypes == null) return null;
+        return propertyTypes.stream()
+                .map(type -> {
+                    try {
+                        return PropertyType.valueOf(type);
+                    } catch (IllegalArgumentException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    private PropertyTransferType mapToPropertyTransferType(String propertyTransferType) {
+        if (propertyTransferType == null) return null;
+        try {
+            return PropertyTransferType.valueOf(propertyTransferType);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    private List<ConstructionType> mapToConstructionTypeList(List<String> constructionTypes) {
+        if (constructionTypes == null) return null;
+        return constructionTypes.stream()
+                .map(type -> {
+                    try {
+                        return ConstructionType.valueOf(type);
+                    } catch (IllegalArgumentException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    private List<OwnershipType> mapToOwnershipTypeList(List<String> ownershipTypes) {
+        if (ownershipTypes == null) return null;
+        return ownershipTypes.stream()
+                .map(type -> {
+                    try {
+                        return OwnershipType.valueOf(type);
+                    } catch (IllegalArgumentException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    public PropertyDTO createProperty(PropertyDTO propertyDTO) {
+        Property property = propertyDTOToProperty(propertyDTO);
+        String imageUUID = randomUUID().toString();
+        String imageName = imageService.uploadImage(propertyDTO.getMainImageBytes(), imageUUID);
+        property.setImageUrl(imageName);
+        List<String> heatingTypes = new ArrayList<>();
+        if (propertyDTO.getHeating() != null) {
+            heatingTypes.addAll(propertyDTO.getHeating());
+        }
+        if (propertyDTO.getHeatingText() != null && !propertyDTO.getHeatingText().trim().isEmpty()) {
+            List<String> additionalHeating = splitHeatingText(propertyDTO.getHeatingText());
+            heatingTypes.addAll(additionalHeating);
+        }
+        property.setHeating(heatingTypes);
+        Property savedProperty = propertyRepository.save(property);
+        propertyImageService.createPropertyImage(propertyDTO.getImages(), savedProperty);
+        return modelMapper.map(savedProperty, PropertyDTO.class);
+    }
+
+    protected List<String> splitHeatingText(String heatingText) {
+        if (heatingText == null || heatingText.trim().isEmpty()) {
+            return new ArrayList<>();
+        }
+        Pattern pattern = Pattern.compile("[^;,\\s]{1,100}");
+        Matcher matcher = pattern.matcher(heatingText);
+
+        List<String> heatingTypes = new ArrayList<>();
+        while (matcher.find()) {
+            heatingTypes.add(matcher.group().trim());
+        }
+        return heatingTypes;
+    }
+
+
+}
 
 
 
