aef513e0be2247c9e1e741ab6e081d6114e2a716
TodorYanakiev <todoryanakiev395@gmail.com>
2024-12-08 21:11:11 +0200

    Find user by email instead of id

--- src/main/java/com/example/ludogorieSoft/village/services/EmailSenderService.java
+++ src/main/java/com/example/ludogorieSoft/village/services/EmailSenderService.java
@@ -76,7 +76,7 @@ public class EmailSenderService {
         LocalDateTime expiryDate = token.getExpiryDate().minusMinutes(15);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm");
         String formattedDate = expiryDate.format(formatter);
-        String resetPasswordLink = "http://localhost:8087/auth/reset-password?token=" + token.getToken() + "&userId=" + user.getId();
+        String resetPasswordLink = "http://localhost:8087/auth/reset-password-form?token=" + token.getToken() + "&userId=" + user.getId();
         return "<div style='text-align: center;'>"
                 + "<a href='https://villagelife.bg'>"
                 + "<img src='cid:logoImage' style='width: 200px;' alt='Site Logo'/>"
