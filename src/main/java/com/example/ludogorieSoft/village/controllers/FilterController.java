ca3011923b715fce1a3828204a7e4182c2bb9729
Gyokhan <gyokhan28@gmail.com>
2025-01-24 14:37:40 +0200

    Fix issue with DI

--- src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
+++ src/main/java/com/example/ludogorieSoft/village/controllers/FilterController.java
@@ -5,21 +5,26 @@ import com.example.ludogorieSoft.village.dtos.VillageDTO;
 import com.example.ludogorieSoft.village.enums.Children;
 import com.example.ludogorieSoft.village.services.PropertyService;
 import com.example.ludogorieSoft.village.services.VillageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/filter")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class FilterController {
-    private VillageService villageSearchService;
     private final PropertyService propertyService;
+    private final VillageService villageSearchService;
 
     @GetMapping("/searchVillages")
     public ResponseEntity<Page<VillageDTO>> searchVillagesByCriteria(
