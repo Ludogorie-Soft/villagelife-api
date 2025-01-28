e642092cb1b47dd00b7e30d8a438d6060dacac9f
Gyokhan <gyokhan28@gmail.com>
2025-01-28 21:23:46 +0200

    Change slack message's client injection

--- src/main/java/com/example/ludogorieSoft/village/slack/SlackMessage.java
+++ src/main/java/com/example/ludogorieSoft/village/slack/SlackMessage.java
@@ -3,20 +3,26 @@ package com.example.ludogorieSoft.village.slack;
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+
 @Component
 @Slf4j
 public class SlackMessage {
+
+    private final MethodsClient client;
+
     @Value("${slack.token}")
-    private  String SLACK_TOKEN;
+    private String SLACK_TOKEN;
+
+    public SlackMessage(MethodsClient client) {
+        this.client = Slack.getInstance().methods();
+    }
 
     public void publishMessage(String channelName, String message) {
-        MethodsClient client = Slack.getInstance().methods();
         try {
             client.chatPostMessage(r -> r
                     .token(SLACK_TOKEN)
