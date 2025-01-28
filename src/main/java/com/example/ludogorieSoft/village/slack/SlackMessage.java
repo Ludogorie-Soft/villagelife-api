f091e195fe87f0c6341fe1b116c29fc51832094e
Gyokhan <gyokhan28@gmail.com>
2025-01-28 21:50:41 +0200

    Add slackconfig for the bug

--- src/main/java/com/example/ludogorieSoft/village/slack/SlackMessage.java
+++ src/main/java/com/example/ludogorieSoft/village/slack/SlackMessage.java
@@ -3,7 +3,6 @@ package com.example.ludogorieSoft.village.slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,9 +12,6 @@ import java.io.IOException;
 public class SlackMessage {
 
     private final MethodsClient client;
-    
-    @Value("${slack.token}")
-    private String SLACK_TOKEN;
 
     public SlackMessage(MethodsClient client) {
         this.client = client;
@@ -24,7 +20,6 @@ public class SlackMessage {
     public void publishMessage(String channelName, String message) {
         try {
             client.chatPostMessage(r -> r
-                    .token(SLACK_TOKEN)
                     .channel(channelName)
                     .text(message)
             );
